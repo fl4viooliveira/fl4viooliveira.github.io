4b75ed7cb4cb2733b1ff00cd7dad8e41b8751a95
fl4viooliveira <fl4viooliveira@gmail.com>
2021-07-31 14:45:17 +0100

    Setup the responsivety

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,13 @@
-function Footer (){
-  return(
-    <h1>Footer</h1>
-  )
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <h1>
+      <div className="copyright">
+        <p>MIT License. Copyright © 2021 Flavio Oliveira.</p>
+      </div>
+    </h1>
+  );
 }
 
 export default Footer;
