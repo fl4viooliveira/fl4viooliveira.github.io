91d35d6967d247db67186c781c870cd8ce0c9d0f
fl4viooliveira <fl4viooliveira@gmail.com>
2021-07-31 12:38:29 +0100

    Added Services Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Home from "./Pages/Home";
 import Header from "./Components/Header";
 import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Work from "./Pages/Work";
+import Contact from "./Pages/Contact";
 import Footer from "./Components/Footer";
 import "./App.css";
+import { ContactShadows } from "@react-three/drei";
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
       <Header />
       <Home />
       <About />
+      <Services />
+      <Work />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/Pages/Services.js
@@ -0,0 +1,81 @@
+import webdevelopment from "../static/images/webdevelopment.png";
+import webdesign from "../static/images/webdesign.png";
+import frontend from "../static/images/frontend.png";
+import backend from "../static/images/backend.png";
+import deployment from "../static/images/deployment.png";
+import administrator from "../static/images/administrator.png";
+import "./Services.css";
+
+function Services() {
+  return (
+    <div>
+      <section class="services" id="services">
+        <div class="title">
+          <h2>Our Services</h2>
+          <p>
+            It is the services that we offer to help you bring your project to
+            life. We can do any step separately or the whole process from
+            scratch to the deployment.
+          </p>
+        </div>
+        <div class="content">
+          <div class="servicesBx">
+            <img src={webdevelopment} alt="Web Development icon" />
+            <h2>Web Development</h2>
+            <p>
+              We develop intuitive software solutions for businesses. This helps
+              to streamline, improve, and convert across a range of web based
+              applications.
+            </p>
+          </div>
+          <div class="servicesBx">
+            <img src={webdesign} alt="Web Development icon" />
+            <h2>Web Design</h2>
+            <p>
+              By creating the perfect visual identity for your project, we aim
+              to attract the attention of your audience with much more than just
+              a logo refresh.
+            </p>
+          </div>
+          <div class="servicesBx">
+            <img src={frontend} alt="Frontend icon" />
+            <h2>Frontend</h2>
+            <p>
+              We’ll guarantee your website works over a range of popular
+              devices. Whether you need to function on tablets or mobile, we
+              have the skills to make it happen.
+            </p>
+          </div>
+          <div class="servicesBx">
+            <img src={backend} alt="Backend icon" />
+            <h2>Backend</h2>
+            <p>
+              Our backend applications usually are scalable and structured in
+              one way to give you the possibility to grow with your project,
+              adding easily new functionalities.
+            </p>
+          </div>
+          <div class="servicesBx">
+            <img src={deployment} alt="Deployment icon" />
+            <h2>Deployment</h2>
+            <p>
+              We can deploy and administrate your applications on clouds
+              services like AWS, Azure, Google Cloud and others.{" "}
+            </p>
+          </div>
+          <div class="servicesBx">
+            <img src={administrator} alt="Administrator icon" />
+            <h2>Website Administration</h2>
+            <p>
+              If something has broken on your website, it needs to be fixed –
+              fast. You’ll have someone on hand at all times, ready to resolve
+              any issues that arise quickly and efficiently.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Services;
